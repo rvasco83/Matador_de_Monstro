1bc2c2a931627f8c202d7c5096d92fa0af58fbaf
r.vasco83 <r.vasco83@hotmail.com>
2020-07-03 17:22:30 +0100

    Implementando Curar

--- app.js
+++ app.js
@@ -20,17 +20,23 @@ new Vue({
             this.hurt('monsterLife', 5, 10, especial)
             this.hurt('playerLife', 7, 12, false)
         },        
-        hurt(prop,mim, max, especial) {
+        hurt(prop,min, max, especial) {
             const plus = especial ? 5 : 0
-            const hurt = this.getRandom(mim + plus, max + plus)
+            const hurt = this.getRandom(min + plus, max + plus)
             this[prop] = Math.max(this[prop] - hurt, 0)
+        },
+        healAndHurt(min, max, especial) {
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
-        },
-
-
+        }
     },
     watch: {
         hasResult(value) {
